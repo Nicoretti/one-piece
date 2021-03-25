29c9e6c09d63e1c35fb8248ff99971f719708e05
Nicola Coretti <nico.coretti@gmail.com>
2021-03-25 22:14:51 +0100

    Update dependencies for bricks library

--- bricks/src/bin/timeout.rs
+++ bricks/src/bin/timeout.rs
@@ -1,15 +1,16 @@
 #[macro_use]
-extern crate clap;
-#[macro_use]
-extern crate human_panic;
-extern crate tokio;
-extern crate tokio_process;
+use clap::{value_t};
+use human_panic::setup_panic;
+use tokio;
+use tokio::process::Child;
+use tokio::runtime;
+use tokio::time::timeout;
+use tokio_process;
 
 use std::process::{exit, Command};
 use std::time::Duration;
 
-use tokio::prelude::Future;
-use tokio::prelude::FutureExt;
+use std::future::Future;
 use tokio_process::CommandExt;
 
 pub const SUCCESS: i32 = 0;
@@ -38,13 +39,14 @@ mod cli {
     }
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     setup_panic!();
     let matches = cli::create_arg_parser().get_matches();
     let duration = value_t!(matches.value_of("duration"), u64).unwrap_or_else(|e| e.exit());
     let mut command = match matches.subcommand() {
         (name, Some(matches)) => {
-            let mut c = Command::new(name);
+            let mut c = tokio::process::Command::new(name);
             let args = matches
                 .values_of("")
                 .map(|v| v.collect())
@@ -60,35 +62,30 @@ fn main() {
         }
     };
 
-    match command.spawn_async() {
+    match command.spawn() {
         Err(e) => {
             eprintln!("Error while constructing command, details:\n {}", e);
         }
         Ok(child) => {
-            let timed_command = child
-                .wait_with_output()
-                .timeout(Duration::from_secs(duration));
+            let timed_command = timeout(Duration::from_secs(duration), child.wait_with_output());
             let future = timed_command
+                .await
                 .map(|r| {
                     // TODO NiCo: find a good alterantive for the unwrap
-                    eprint!("{}", std::str::from_utf8(&r.stderr).unwrap());
-                    print!("{}", std::str::from_utf8(&r.stdout).unwrap());
-                    exit(SUCCESS);
+                    r.map(|output| {
+                        eprint!("{}", std::str::from_utf8(&output.stderr).unwrap());
+                        print!("{}", std::str::from_utf8(&output.stdout).unwrap());
+                        exit(SUCCESS);
+                    })
+                    .map_err(|e| {
+                        eprintln!("Error while executing command, details: {}", e);
+                        exit(ERROR);
+                    })
                 })
                 .map_err(|e| {
-                    let exit_code = match e.is_elapsed() {
-                        true => {
-                            eprintln!("Timeout, command did not finish in time");
-                            TIMEOUT
-                        }
-                        false => {
-                            eprintln!("Command execution failed");
-                            ERROR
-                        }
-                    };
-                    exit(exit_code);
+                    eprintln!("Timeout, command did not finish in time");
+                    exit(TIMEOUT);
                 });
-            tokio::run(future);
         }
     }
 }
