2eae023492386dde4fb519f0cf4b94c69054ebba
Nicola Coretti <nico.coretti@gmail.com>
2021-08-01 14:48:52 +0200

    Add first working version ringlight-updater

--- elgato/src/bin/ringlight-updater.rs
+++ elgato/src/bin/ringlight-updater.rs
@@ -1,7 +1,10 @@
-use anyhow::{Error, Result};
+use anyhow::{format_err, Result};
 use human_panic::setup_panic;
+use itertools::Itertools;
+use reqwest::{ClientBuilder, StatusCode};
+use std::io::Read;
 use structopt::StructOpt;
-use tokio;
+use tokio::time::{sleep, Duration};
 
 mod cli {
     use std::net::IpAddr;
@@ -23,12 +26,29 @@ mod cli {
         pub ip: IpAddr,
 
         #[structopt(
+            name = "ms",
             short = "d",
             long = "delay",
-            default_value = "50",
+            default_value = "25",
             help = "inter packet delay in milli seconds"
         )]
-        pub delay: usize,
+        pub delay: u64,
+
+        #[structopt(
+            name = "bytes",
+            short = "c",
+            long = "chunk-size",
+            default_value = "4096",
+            help = "size of individually transferred chunks in bytes"
+        )]
+        pub chunk_size: usize,
+
+        #[structopt(
+            name = "count",
+            long = "chunk-retries",
+            help = "Amount of retires for a single chunk upload, if not specified it will retry until chunk upload succeeds"
+        )]
+        pub chunk_retries: Option<usize>,
     }
 }
 
@@ -36,14 +56,71 @@ mod cli {
 async fn main() -> Result<()> {
     setup_panic!();
     let args = cli::Updater::from_args();
-    // 1. determine firmware size
-    // 2. send initial update info to start update (endpoint ...)
-    // 3. while remaining size > 0: send next chunk to chunk endpoint including chunk size
-    // 4. wait delay
-    // 5. go to 3
+    let firmware = std::fs::File::open(args.file)?;
 
-    // TODO:
-    // 1. Figure out max timeout delay for update process
-    // 2. Get a trace from a successful update to determine if final packet or so needs to to something special
-    Ok(())
+    let client = ClientBuilder::new().gzip(true).deflate(true).build()?;
+
+    // 1. determine firmware size
+    let size = firmware.metadata()?.len();
+
+    // 2. start firmware update upload and signal firmware size
+    let prepare_update = client
+        .put(format!(
+            "http://{}:9123/elgato/firmware-update/prepare",
+            args.ip
+        ))
+        .body(format!("{{\"size\":{}}}", size))
+        .build()?;
+
+    client.execute(prepare_update).await?;
+
+    // 3. while remaining size > 0: send next chunk to chunk endpoint including chunk size
+    let mut offset: usize = 0;
+    for chunk in firmware.bytes().chunks(args.chunk_size).into_iter() {
+        let data: Vec<u8> = chunk.filter_map(|r| r.ok()).collect();
+        let size = data.len();
+
+        let mut success = false;
+        let mut retries = 0usize;
+        while !success {
+            // maximum retries for a chunk reached
+            if let Some(max_retries) = args.chunk_retries {
+                if retries >= max_retries {
+                    return Err(format_err!(
+                        "Reached the maximum amount of retries for a chunk, details: offset={}",
+                        offset
+                    ));
+                }
+                retries += 1;
+            }
+
+            let upload_data = client
+                .put(format!(
+                    "http://{}:9123/elgato/firmware-update/data?offset={}",
+                    args.ip, offset,
+                ))
+                .body(data.clone())
+                .build()?;
+            let status = client.execute(upload_data).await?.status();
+            if status == StatusCode::ACCEPTED {
+                success = true;
+            } else {
+                sleep(Duration::from_millis(args.delay)).await;
+            }
+        }
+        offset += size;
+    }
+
+    // 4. tell ring light to execute/install firmware update
+    let execute_update = client
+        .post(format!(
+            "http://{}:9123/elgato/firmware-update/execute",
+            args.ip
+        ))
+        .build()?;
+
+    match client.execute(execute_update).await?.status() {
+        StatusCode::OK => Ok(()),
+        status => Err(format_err!("Update failed, details {}", status)),
+    }
 }
