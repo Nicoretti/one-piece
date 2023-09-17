5bf7ed80b3ad5d66e469a4893c5b1ab3722e86b7
Nicola Coretti <nico.coretti@gmail.com>
2023-09-17 15:09:14 +0200

    Add context manager to clikit

--- rust/clikit/src/lib.rs
+++ rust/clikit/src/lib.rs
@@ -1,14 +1,69 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+mod context {
+
+    pub trait ContextManager {
+        fn enter(self);
+    }
+
+    pub struct Context<T> {
+        inner: T,
+    }
+
+    impl<T: ContextManager> Context<T> {
+        pub fn enter<F>(self, closure: F)
+        where
+            F: FnOnce(T),
+        {
+            println!("Enter");
+            closure(self.inner);
+            drop(self);
+            println!("Exit");
+        }
+    }
+
+    impl<T> Drop for Context<T> {
+        fn drop(&mut self) {
+            todo!()
+        }
+    }
+
+    impl<T: ContextManager> Context<T> {
+        pub fn new(inner: T) -> Self {
+            Self { inner }
+        }
+    }
+
+    pub struct Environment {}
+
+    impl Environment {
+        pub fn add(self) -> Self {
+            self
+        }
+    }
+
+    impl ContextManager for Environment {
+        fn enter(self) {
+            println!("Doing Environment stuff");
+        }
+    }
+
+    impl Drop for Environment {
+        fn drop(&mut self) {
+            println!("DROPPING ENV");
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use crate::context;
+    use crate::context::Context;
+    use crate::context::ContextManager;
+    use crate::context::Environment;
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        Context::<Environment>::new(Environment {}.add().add()).enter(|env| {
+            println!("executing code");
+        })
     }
 }
