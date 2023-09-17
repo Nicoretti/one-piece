968ca658bfc07c1cf14282dd5c06925d3ae0180c
Nicola Coretti <nico.coretti@gmail.com>
2023-09-17 16:09:20 +0200

    Add absValue function

--- haskell/functions.hs
+++ haskell/functions.hs
@@ -9,8 +9,11 @@ fibonacci 0 = 0
 fibonacci 1 = 1
 fibonacci n = fibonacci(n - 1) + fibonacci(n - 2)
 
-a = fibonacci(2)
-
+-- Absolute Value
+absValue :: Int -> Int
+absValue x
+    | x >= 0 = x
+    | otherwise = -x
 
 
 
