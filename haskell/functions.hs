a93d23b96428d22edbf89558f99368163b732525
Nicola Coretti <nico.coretti@gmail.com>
2023-09-17 15:02:16 +0200

    Add haskell file for math functions

---
+++ haskell/functions.hs
@@ -0,0 +1,16 @@
+factorial :: Integer -> Integer
+factorial 0 = 1
+factorial n = n * factorial(n - 1)
+
+
+fibonacci :: Integer -> Integer
+fibonacci 0 = 0
+fibonacci 1 = 1
+fibonacci n = fibonacci(n - 1) + fibonacci(n - 2)
+
+a = fibonacci(2)
+
+
+
+
+
