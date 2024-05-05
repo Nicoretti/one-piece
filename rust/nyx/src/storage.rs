fe14d7dabc689b7bc11b56b4ba95e3f77b6d9dd1
Nicola Coretti <nico.coretti@gmail.com>
2024-05-05 22:24:50 +0200

    Refactor storage

--- rust/nyx/src/storage.rs
+++ rust/nyx/src/storage.rs
@@ -1,13 +1,57 @@
+use anyhow::{bail, Result};
 use indoc::indoc;
 use log::*;
+use rusqlite::{params, Connection, Row};
 use std::path::{Path, PathBuf};
-
-use anyhow::{bail, Result};
-use rusqlite::{params, Connection};
 
 #[derive(Debug)]
 struct Storage {
     db: PathBuf,
+}
+
+const NO_PARAMS : () = ();
+
+impl Storage {
+    const DDL_SESSIONS: &'static str = indoc! {r#"
+            CREATE TABLE IF NOT EXISTS sessions (
+                id INTEGER PRIMARY KEY,
+                name TEXT NOT NULL UNIQUE,
+                model TEXT NOT NULL,
+                temperature REAL NOT NULL
+            );
+        "#};
+
+    const DDL_MESSAGES: &'static str = indoc! {r#"
+            CREATE TABLE IF NOT EXISTS messages (
+                id INTEGER PRIMARY KEY,
+                role TEXT NOT NULL UNIQUE,
+                content TEXT NOT NULL,
+                session_id  INTEGER,
+                FOREIGN KEY(session_id) REFERENCES sessions(id)
+            );
+        "#};
+
+    fn init(&self) -> Result<()> {
+        debug!("Initializing database ({:?})", self.db);
+        let con = Connection::open(&self.db)?;
+        con.execute(Self::DDL_SESSIONS, NO_PARAMS)?;
+        con.execute(Self::DDL_MESSAGES, NO_PARAMS)?;
+        Ok(())
+    }
+
+    pub fn new<P: AsRef<Path>>(path: P) -> Result<Self> {
+        let storage = Self {
+            db: path.as_ref().to_path_buf(),
+        };
+        storage.init()?;
+        Ok(storage)
+    }
+
+    pub fn session(&self) -> SessionBuilder {
+        SessionBuilder {
+            db: self.db.clone(),
+        }
+    }
 }
 
 #[derive(Debug, PartialEq)]
@@ -18,47 +62,46 @@ struct Session {
     temperature: f32,
 }
 
+impl Session {
+    fn from_row(row: &Row) -> rusqlite::Result<Session> {
+        Ok(Self {
+            id: row.get(0)?,
+            name: row.get(1)?,
+            model: row.get(2)?,
+            temperature: row.get(3)?,
+        })
+    }
+}
+
 struct SessionBuilder {
     db: PathBuf,
 }
 
 impl SessionBuilder {
-    pub fn new<P: AsRef<Path>>(path: P) -> Result<Self> {
-        Ok(Self {
-            db: path.as_ref().to_path_buf(),
-        })
-    }
-
     pub fn load(&self, id: u64) -> Result<Session> {
         let con = Connection::open(&self.db)?;
-        debug!("Loading session ({}) from the database.", id);
-        Ok(
-            con.query_row("SELECT * FROM sessions WHERE id = ?;", (id,), |row| {
-                Ok(Session {
-                    id: row.get(0)?,
-                    name: row.get(1)?,
-                    model: row.get(2)?,
-                    temperature: row.get(3)?,
-                })
-            })?,
-        )
+        let session: Session = con.query_row(
+            "SELECT * FROM sessions WHERE id = ?;",
+            params![id],
+            Session::from_row,
+        )?;
+        Ok(session)
     }
 
-    fn create(&self, name: &str, model: &str, temperature: f32) -> Result<Session> {
+    pub fn create(&self, name: &str, model: &str, temperature: f32) -> Result<Session> {
         let con = Connection::open(&self.db)?;
-        let mut id = con.query_row("SELECT COALESCE(MAX(id), 0) FROM sessions;", (), |row| {
-            row.get(0)
-        })?;
-        id += 1;
+        let id: u64 = con.query_row(
+            "SELECT COALESCE(MAX(id), 0) FROM sessions;",
+            NO_PARAMS,
+            |row| row.get(0),
+        )?;
 
         let session = Session {
-            id,
+            id: id + 1,
             name: name.into(),
             model: model.into(),
             temperature,
         };
-
-        debug!("Creating new session, details: {:?}", session);
         con.execute(
             "INSERT INTO sessions(id, name, model, temperature) VALUES(?, ?, ?, ?);",
             params![session.id, session.name, session.model, session.temperature],
@@ -68,88 +111,28 @@ impl SessionBuilder {
     }
 }
 
-impl Storage {
-    const DDL_SESSIONS: &'static str = indoc! {
-    r#"
-            CREATE TABLE IF NOT EXISTS sessions (
-                id INTEGER PRIMARY KEY,
-                name TEXT NOT NULL UNIQUE,
-                model TEXT NOT NULL,
-                temperature REAL NOT NULL
-            );
-        "#};
-
-    const DDL_MESSAGES: &'static str = indoc! {
-        r#"
-            CREATE TABLE IF NOT EXISTS messages (
-                id INTEGER PRIMARY KEY,
-                role TEXT NOT NULL UNIQUE,
-                content TEXT NOT NULL,
-                session_id  INTEGER,
-                FOREIGN KEY(session_id) REFERENCES sessions(id)
-            );
-        "#
-    };
-
-    fn init(&self) -> Result<()> {
-        debug!("Initializing DB {:?}.", self.db);
-        let con = Connection::open(&self.db)?;
-        con.execute(Storage::DDL_SESSIONS, ())?;
-        con.execute(Storage::DDL_MESSAGES, ())?;
-        Ok(())
-    }
-
-    pub fn new<P: AsRef<Path>>(path: P) -> Result<Self> {
-        let storage = Self {
-            db: path.as_ref().to_path_buf(),
-        };
-        if let Err(e) = storage.init() {
-            bail!("Could not initialize DB: {}", e);
-        }
-        Ok(storage)
-    }
-
-    pub fn session(&self) -> Result<SessionBuilder> {
-        SessionBuilder::new(self.db.clone())
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
     use std::sync::Once;
+
     static INIT: Once = Once::new();
-
     pub fn initialize() {
-        INIT.call_once(|| {
-            env_logger::init();
-        });
+        INIT.call_once(|| env_logger::init());
     }
 
     #[test]
-    fn test_create_new_storage() -> Result<()> {
+    fn init() {
+        initialize()
+    }
+
+    #[test]
+    fn test_a_new_session_can_be_created() -> anyhow::Result<()> {
         let db = tempfile::Builder::new()
             .prefix("test")
             .suffix(".db")
             .tempfile()?;
-
-        Storage::new(db.path())?;
-
-        let con = Connection::open(db.path()).expect("Could not open DB.");
-        let res = con.execute("SELECT * FROM sessions;", ());
-
-        assert!(res.is_ok());
-        Ok(())
-    }
-
-    #[test]
-    fn test_a_new_session_can_be_created() -> Result<()> {
-        let db = tempfile::Builder::new()
-            .prefix("test")
-            .suffix(".db")
-            .tempfile()?;
-        let storage = Storage::new(db.path())?;
-
+        let storage = Storage::new(db.as_ref())?;
         let expected = Session {
             id: 1,
             name: "test-session".into(),
@@ -157,51 +140,45 @@ mod tests {
             temperature: 1.0,
         };
         let actual = storage
-            .session()?
+            .session()
             .create("test-session", "openai:gpt-4", 1.0)?;
-
         assert_eq!(actual, expected);
         Ok(())
     }
 
     #[test]
-    fn test_loading_existing_session() -> Result<()> {
-        initialize();
+    fn test_loading_existing_session() -> anyhow::Result<()> {
         let db = tempfile::Builder::new()
             .prefix("test")
             .suffix(".db")
             .tempfile()?;
-        let storage = Storage::new(db.path())?;
-        {
-            storage
-                .session()?
-                .create("test-session1", "openai:gpt-4", 1.0)?;
-            storage
-                .session()?
-                .create("test-session2", "openai:gpt-4", 0.5)?;
-        }
+        let storage = Storage::new(db.as_ref())?;
+        storage
+            .session()
+            .create("test-session1", "openai:gpt-4", 1.0)?;
+        storage
+            .session()
+            .create("test-session2", "openai:gpt-4", 0.5)?;
+
         let expected = Session {
             id: 2,
             name: "test-session2".into(),
             model: "openai:gpt-4".into(),
             temperature: 0.5,
         };
-
-        let actual = storage.session()?.load(2)?;
-
+        let actual = storage.session().load(2)?;
         assert_eq!(expected, actual);
         Ok(())
     }
 
     #[test]
-    fn test_loading_non_existing_session_returns_an_error() -> Result<()> {
+    fn test_loading_non_existing_session_returns_an_error() -> anyhow::Result<()> {
         let db = tempfile::Builder::new()
             .prefix("test")
             .suffix(".db")
             .tempfile()?;
-        let storage = Storage::new(db.path())?;
-        let result = storage.session()?.load(1);
-
+        let storage = Storage::new(db.as_ref())?;
+        let result = storage.session().load(1);
         assert!(result.is_err());
         Ok(())
     }
