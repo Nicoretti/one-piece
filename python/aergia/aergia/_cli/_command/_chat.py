fd5ff01ceae3690a1c57a2906a2660bf4e3ca1b5
Nicola Coretti <nico.coretti@gmail.com>
2024-05-23 22:15:36 +0200

    Remove stream parameter for chat command

--- python/aergia/aergia/_cli/_command/_chat.py
+++ python/aergia/aergia/_cli/_command/_chat.py
@@ -9,6 +9,9 @@ from aergia._client import build_client, Type
 
 
 def chat(args, stdout, stderr):
+    def is_chunk_valid(chunk):
+        return chunk.choices is not None and len(chunk.choices) > 0
+
     async def async_chat(client, model, message):
         stream = await client.chat.completions.create(
             model=model,
@@ -19,6 +22,7 @@ def chat(args, stdout, stderr):
             "Processing request ...", spinner="aesthetic", spinner_style="cyan"
         ):
             with TextBuffer() as buffer:
+                stream = (c async for c in stream if is_chunk_valid(c))
                 async for chunk in stream:
                     text = chunk.choices[0].delta.content or ""
                     buffer.append(text)
@@ -34,12 +38,13 @@ def chat(args, stdout, stderr):
             "Processing request ...", spinner="aesthetic", spinner_style="cyan"
         ):
             with TextBuffer() as buffer:
+                stream = (c for c in stream if is_chunk_valid(c))
                 for chunk in stream:
                     text = chunk.choices[0].delta.content or ""
                     buffer.append(text)
                 return buffer.content
 
-    client_type = Type.Sync
+    client_type = Type.Async
     api_key = os.environ.get("OPENAI_API_KEY")
     client = build_client(backend="openai", api_key=api_key, client_type=client_type)
     msg = " ".join(args.text)
@@ -75,13 +80,6 @@ def add_chat_subcommand(subparsers):
         default="gpt-4o",
         choices=["gpt-4o", "gpt-4", "gpt-4-turbo", "gpt-3.5-turbo"],
         help="Select a model",
-    )
-    subcommand.add_argument(
-        "--stream",
-        dest="stream",
-        action="store_true",
-        default=True,
-        help="Enable streaming",
     )
     subcommand.set_defaults(func=chat)
     return subcommand
